2560f0fabfcc76e93be4e4eac0b42600a266eea9
username <44939626+mikaggg@users.noreply.github.com>
2021-05-18 00:09:13 +0900

    next

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function HomePage() {
+    return <div>ネクストJS</div>
+}
+
+export default HomePage;
---
+++ pages/other.js
@@ -0,0 +1,8 @@
+function Other() {
+    return <div>
+        <p>ここは別のページ</p>
+        <p><a href="/">戻る</a></p>
+    </div>;
+}
+
+export default Other;
